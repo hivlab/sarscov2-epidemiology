765e04b45425dcf31dc7d601fd31fa8b7d58bf93
Taavi Päll <tapa741@gmail.com>
2020-04-02 06:40:02 +0300

    added data scripts

--- scripts/parse_gb.py
+++ scripts/parse_gb.py
@@ -2,6 +2,7 @@ from Bio import SeqIO
 from collections import OrderedDict, Counter
 import pandas as pd
 from datetime import datetime
+import re
 
 
 def fix_date(x):
@@ -14,30 +15,34 @@ def fix_date(x):
     return fixed
 
 
+sars = "Severe acute respiratory syndrome coronavirus 2 (isolate )?"
+gen = ", complete genome"
+
+
 ord_list = []
 with open("data/sequences.fasta", "w") as fasta_handle:
     for seq_record in SeqIO.parse("data/sequences.gb", "genbank"):
-        q = seq_record.features[0]
-        ids = {"id": seq_record.id, "description": seq_record.description}
-        qualifiers = {k: v[0] for k, v in q.qualifiers.items()}
-        ids.update(qualifiers)
-        ord_list.append(ids)
-        fasta_handle.write(
-            ">{} {}\n{}\n".format(seq_record.id, seq_record.description, seq_record.seq)
-        )
+        if len(seq_record.seq) > 28000:
+            q = seq_record.features[0]
+            ids = {"accession": seq_record.id, "description": seq_record.description, "length": len(seq_record.seq)}
+            qualifiers = {k: v[0] for k, v in q.qualifiers.items()}
+            if "isolate" in qualifiers.keys():
+                ids.update({"strain": qualifiers["isolate"]})
+            if "strain" not in ids.keys():
+                ids.update({"strain": ids["accession"]})
+            ids.update(qualifiers)
+            ord_list.append(ids)
+            fasta_handle.write(
+                ">{}\n{}\n".format(ids["strain"], seq_record.seq)
+            )
+
 
 df = pd.DataFrame(ord_list, columns=ord_list[0].keys())
-df_rename1 = df.rename(columns={"strain": "strain2"})
-df_renamed = df_rename1.rename(
-    columns={"id": "accession", "organism": "virus", "collection_date": "date"}
-)
-df_renamed["strain"] = (
-    df_renamed["description"]
-    .str.replace("Severe acute respiratory syndrome coronavirus 2 (isolate )?", "")
-    .str.replace(", complete genome", "")
+df_renamed = df.rename(
+    columns={"organism": "virus", "collection_date": "date"}
 )
 df_renamed["date"] = df_renamed["date"].apply(lambda x: fix_date(x))
 new = df_renamed["country"].str.split(": ", expand = True)
 df_renamed["division"] = new[1]
 df_renamed["country"] = new[0]
-df_renamed.to_csv("data/metadata.tsv", sep="\t")
+df_renamed.to_csv("data/metadata.tsv", sep="\t", index=False)
